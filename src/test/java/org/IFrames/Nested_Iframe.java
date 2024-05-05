329d00000b928536d94eaacca89c2950f3dd9d42
Noraiz <noraizmaher@gmail.com>
2024-05-05 11:38:49 +0400

    Nested IFrames

---
+++ src/test/java/org/IFrames/Nested_Iframe.java
@@ -0,0 +1,29 @@
+package org.IFrames;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Nested_Iframe {
+    static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.dezlearn.com/nested-iframes-example/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        // Switch to parent Frame
+        driver.switchTo().frame("demo_parent_iframe");
+
+        // Switch to Child Frame
+        driver.switchTo().frame("demo_frame1");
+        driver.findElement(By.xpath("//button[@id='u_5_6']")).click();
+
+        // Switch Back to Parent Frame
+        driver.switchTo().parentFrame();
+        driver.findElement(By.xpath("//button[@id='u_5_5']")).click();
+
+    }
+}
