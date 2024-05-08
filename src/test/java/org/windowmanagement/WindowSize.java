1241419835c1d9f0340662764379610c489d8e00
Noraiz <noraizmaher@gmail.com>
2024-05-08 20:27:37 +0400

    Get Window Size

---
+++ src/test/java/org/windowmanagement/WindowSize.java
@@ -0,0 +1,35 @@
+package org.windowmanagement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowSize {
+    static WebDriver driver;
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.goodreads.com/");
+
+
+        /* First Way
+        int width = driver.manage().window().getSize().getWidth();
+        int height = driver.manage().window().getSize().getWidth();
+        System.out.println("Width : "+ width + " Height : "+ height);*/
+
+        /* Second way
+        Dimension size  = driver.manage().window().getSize();
+        int width = size.width;
+        int height = size.height;
+        System.out.println("Width : "+ width + " Height : "+ height);*/
+
+        // Third Way
+        Dimension size  = driver.manage().window().getSize();
+        int width = size.getWidth();
+        int height = size.getHeight();
+        System.out.println("Width : "+ width + " Height : "+ height);
+
+
+    }
+}
